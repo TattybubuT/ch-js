60a1761d07a872a247834804519464b2486a2018
TattybubuT <tattybubu.talantbekova.kk@gmail.com>
2022-03-06 17:25:10 +0600

    loops added while and for

---
+++ v14_loops/app.js
@@ -0,0 +1,17 @@
+// loops
+const num = 50;
+while (num <= 55){
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);   // this will run at least once
+
+// FOR LOOPS
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
